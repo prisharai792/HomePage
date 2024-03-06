159ec1a4bb7814c61ea46d5af8ecebb62fb9d9eb
Prisha <prisharai792@gmail.com>
2024-03-07 03:16:34 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import HeroSlider from "../components/UI/HeroSlider";
+import Helmet from "../components/Helmet/Helmet";
+
+import { Container, Row, Col } from "reactstrap";
+import FindCarForm from "../components/UI/FindCarForm";
+import AboutSection from "../components/UI/AboutSection";
+import BecomeDriverSection from "../components/UI/BecomeDriverSection";
+
+const Home = () => {
+  return (
+    <Helmet title="Home">
+      {/* ============= hero section =========== */}
+      <section className="p-0 hero__slider-section">
+        <HeroSlider />
+
+        <div className="hero__form">
+          <Container>
+            <Row className="form__row">
+              <Col lg="4" md="4">
+                <div className="find__cars-left">
+                  <h2>Find your best car here</h2>
+                </div>
+              </Col>
+
+              <Col lg="8" md="8" sm="12">
+                <FindCarForm />
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </section>
+      {/* =========== about section ================ */}
+      <AboutSection />
+      
+      
+      {/* =========== become a driver section ============ */}
+      <BecomeDriverSection />
+
+    </Helmet>
+  );
+};
+
+export default Home;
